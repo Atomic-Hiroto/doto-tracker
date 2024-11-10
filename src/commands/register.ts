cdb97bd1af94f42bd40a21d3b0fb37ca582a9310
Anish Deshpande <nitrawf@gmail.com>
2024-11-10 17:14:47 +0530

    Enable forced registrations

--- src/commands/register.ts
+++ src/commands/register.ts
@@ -1,17 +1,24 @@
 import { Message } from 'discord.js';
 import { Replies } from '../constants';
 import { UserDataService } from '../services/userDataService';
-import { isValidSteamId } from '../utils/validators';
+import { isValidDiscordId, isValidSteamId } from '../utils/validators';
+import { logger } from '../services/loggerService';
 
 export async function register(message: Message, args: string[], userDataService: UserDataService) {
-  if (args.length !== 1) {
+  if (args.length < 1) {
     return message.reply(Replies.PROVIDE_STEAM_ID);
   }
 
   const steamId = args[0];
+  const discordId = args.length > 1 ? (extractUserId(args[1]) ? extractUserId(args[1]) : args[1]) : message.author.id;
+  logger.debug(discordId);
 
   if (!isValidSteamId(steamId)) {
     return message.reply('Invalid Steam ID. Please provide a valid 32-bit Steam ID.');
+  }
+
+  if (!isValidDiscordId(discordId)) {
+    return message.reply('Invalid discord ID. Please provide a valid discord ID.')
   }
 
   const existingUser = userDataService.getUserBySteamId(steamId);
@@ -20,11 +27,16 @@ export async function register(message: Message, args: string[], userDataService
   }
 
   userDataService.addUser({
-    discordId: message.author.id,
+    discordId: discordId,
     steamId: steamId,
     autoShow: true,
     lastCheckedMatch: null
   });
 
   message.reply(Replies.REGISTER_SUCCESS(steamId));
+}
+
+function extractUserId(mention: string): string {
+  const match = mention.match(/\d+/);
+  return match?.[0] ?? '';
 }
--- src/utils/validators.ts
+++ src/utils/validators.ts
@@ -3,3 +3,17 @@ export function isValidSteamId(steamId: string): boolean {
   // Steam32 IDs are typically 8-10 digits long
   return /^[0-9]{8,10}$/.test(steamId);
 }
+
+export function isValidDiscordId(discordUserId: string) {
+  // Discord user IDs are 17 or 18 digits long
+  if (discordUserId.length !== 17 && discordUserId.length !== 18) {
+    return false;
+  }
+
+  // Check if the ID consists of only digits
+  if (!/^\d+$/.test(discordUserId)) {
+    return false;
+  }
+
+  return true;
+}
